bf1854470451410c14217a783f52ab336a7cfc3c
Ben Hentschel <124124446+Bheny07@users.noreply.github.com>
2023-05-03 11:37:33 +1200

    Component 2: v1

---
+++ Component_2_Establishing_quiz_v2.py
@@ -0,0 +1,18 @@
+"""Adding """
+
+
+def get_difficulty_level():
+    while True:
+        response = input("Select a difficulty level (1-3): ")
+        if response.isdigit() and 1 <= int(response) <= 3:
+            return int(response)
+        else:
+            print("Invalid input. Please enter a number between 1 and 3.")
+
+
+# Call the function to ask the user for the difficulty level
+difficulty_level = get_difficulty_level()
+
+# Use the value of difficulty_level to determine what to do next
+print(f"You selected difficulty level {difficulty_level}. Let's start the "
+      f"quiz!")
