1f14e6f94db14e8f9f760215b8537bc41bfadcec
Ben Hentschel <124124446+Bheny07@users.noreply.github.com>
2023-05-03 17:07:49 +1200

    Updating Base Component with Component 2 v2

--- Base Component.py
+++ Base Component.py
@@ -1,12 +1,12 @@
 
 # Function to ask if User has played the quiz before
 def quiz_played_before():
-    response = input("Have you played the quiz before? Enter 'yes' or 'no': ")
+    response1 = input("Have you played the quiz before? Enter 'yes' or 'no': ")
 
-    if response.lower() in ["yes", "y"]:
+    if response1.lower() in ["yes", "y"]:
         print("Welcome back!")
         return True
-    elif response.lower() in ["no", "n"]:
+    elif response1.lower() in ["no", "n"]:
         print("Great, let's get started!\nThis is a quiz on your knowledge of"
               "Maori numbers\nThese are the Instructions:\n1. Select your "
               "difficulty from 1-3 with 3 being the hardest.\n2. "
@@ -22,5 +22,29 @@ def quiz_played_before():
         return quiz_played_before()
 
 
+def get_difficulty_level():
+    while True:
+        response1 = input("Select a difficulty level (1-3): ")
+        if response1.isdigit() and 1 <= int(response1) <= 3:
+            return int(response1)
+        else:
+            print("Invalid input. Please enter a number between 1 and 3.")
+
+
 # Call the function to ask the user if they have played the quiz before
 played_before = quiz_played_before()
+
+# Prompt the user to select a difficulty level
+print("Welcome to the quiz!")
+while True:
+    difficulty_level = get_difficulty_level()
+    print(f"You selected difficulty level {difficulty_level}.")
+    response = input("Would you like to start the quiz with this difficulty "
+                     "level? (yes/no) ")
+    if response.lower() in ["yes", "y"]:
+        break
+    elif response.lower() in ["no", "n"]:
+        print("Okay, let's try again.")
+    else:
+        print("Invalid input. Please enter 'yes' or 'no'.")
+print("Starting the quiz...")
